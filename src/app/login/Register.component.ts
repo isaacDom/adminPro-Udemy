38fac07c033dcd1700846496a059b27fdac23998
isaacDom <isaac.d.rinconmail.com>
2020-03-24 20:08:28 -0600

    primer commit

---
+++ src/app/login/Register.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-login',
+  templateUrl: './register.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class RegisterComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+  }
+}
+
